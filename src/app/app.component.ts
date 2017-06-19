fadc08ad93afafc59c21f741e3f7e16552f3a368
Joanna Koterba <joannakoterba@Mac-mini-Adam.local>
2017-06-19 16:30:13 +0200

    create new project - start animation page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import WebFont from 'webfontloader';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    WebFont.load({
+      google: {
+        families: ['Proxima Nova']
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { StartAnimationComponent } from './main/start-animation/start-animation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    StartAnimationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
