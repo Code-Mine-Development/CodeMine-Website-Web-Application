38e3e3ec7281bc01b9a819d9194c0f9557fa7022
Kamil Zajac <zajackamil1@gmail.com>
2017-06-21 16:34:37 +0200

    portfolio process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProjectsComponent } from './portfolio/projects/projects.component';
 import { ProjectDetailComponent } from './portfolio/projects/project-detail/project-detail.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +27,7 @@ import { ProjectDetailComponent } from './portfolio/projects/project-detail/proj
     NavigationComponent,
     ProjectsComponent,
     ProjectDetailComponent,
+
 
   ],
   imports: [
