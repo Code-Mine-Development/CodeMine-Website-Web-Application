2d81f540407a1189f0b206d6e3cc0b2b11d7ebbd
matusik <m.s.matusik@gmail.com>
2017-07-04 15:38:34 +0200

    dragable

--- src/app/components/about-us/about-us-components.module.ts
+++ src/app/components/about-us/about-us-components.module.ts
@@ -2,16 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DeskComponent} from "./desk/desk.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ArrowComponent} from "./arrow/arrow.component";
 
 @NgModule({
   declarations: [
-    DeskComponent
+    DeskComponent,
+    ArrowComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    DeskComponent
+    DeskComponent,
+    ArrowComponent
   ],
   providers: [],
 })
