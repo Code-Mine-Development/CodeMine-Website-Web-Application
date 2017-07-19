ba58959ba70804f673dafc8c79aa4119bb6f2e29
mateuszwit <maateew@gmail.com>
2017-07-19 10:19:45 +0200

    Dynamic Page Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {Router, NavigationEnd} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
 import {LocalizeRouterService} from 'localize-router';
 
@@ -9,23 +11,28 @@ import {LocalizeRouterService} from 'localize-router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private translate: TranslateService, private localize:LocalizeRouterService) {
-    const languages = <[string]>localize.parser.locales;
-    const browserLang = translate.getBrowserLang();
-    const currentLang = localize.parser.currentLang;
+  constructor(private translate: TranslateService, private localize:LocalizeRouterService, private titleService:Title, private router:Router) {
+    this.selectLanguage();
+    this.changeTitle();
+  }
+
+  selectLanguage(){
+    const languages = <[string]>this.localize.parser.locales;
+    const browserLang = this.translate.getBrowserLang();
+    const currentLang = this.localize.parser.currentLang;
     const defaultLang = this.checkLanguages( browserLang, languages )? browserLang : 'pl';
 
 
-    translate.addLangs(languages);
+    this.translate.addLangs(languages);
 
     if( this.checkLanguages( currentLang, languages ) )
-      this.selectLanguage(currentLang);
+      this.activeLanguage(currentLang);
     else
-      this.selectLanguage(defaultLang);
-
+      this.activeLanguage(defaultLang);
   }
 
-  selectLanguage(lang){
+
+  activeLanguage(lang){
       this.translate.use(lang);
       this.translate.setDefaultLang(lang);
   }
@@ -34,6 +41,33 @@ export class AppComponent implements OnInit {
     return languages.find( (listLang) => (listLang == lang)) || false;
   }
 
+  changeTitle(){
+    let prefix = "",
+        page = "";
+
+    this.translate.get('PAGETITLE.prefix').subscribe( (translation) => {
+      prefix = translation;
+      this.setTitle(prefix, page);
+    });
+
+    this.router.events.subscribe( (state) => {
+      if(state instanceof NavigationEnd)
+        page = this.parseUrl(state.url);
+      this.setTitle(prefix, page);
+    });
+
+  }
+
+  parseUrl(url:string){
+    let urlPartsList = url.split("/"),
+        subPage = urlPartsList[urlPartsList.length-1];
+
+    return subPage.replace("-"," ");
+  }
+
+  setTitle(prefix:string, page:string){
+    this.titleService.setTitle(prefix + " | " + page);
+  }
 
   ngOnInit() {
 
