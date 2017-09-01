d283c45c0062595ac57aeaaa9af9012795870345
mateuszwit <maateew@gmail.com>
2017-09-01 16:26:21 +0200

    Fixxx

--- src/app/components/audit/audit-components.module.ts
+++ src/app/components/audit/audit-components.module.ts
@@ -8,13 +8,15 @@ import {TranslateModule} from '@ngx-translate/core';
 
 import {DrawBackgroundService} from '../../shared/services/draw-background.service';
 import {AuditListElementComponent} from './audit-list/audit-list-element.component';
+import {AuditHeadComponent} from './audit-head/audit-head.component';
 
 
 @NgModule({
   declarations: [
     AuditDetailsComponent,
     AuditListComponent,
-    AuditListElementComponent
+    AuditListElementComponent,
+    AuditHeadComponent
   ],
   imports: [
     CommonModule,
