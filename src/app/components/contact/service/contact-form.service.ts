830b05ec6fa28a1ec2fed1069b3645dbb6d7f2b8
Radek Adamiec <radek@adamiec.it>
2017-09-19 17:14:19 +0200

    Small improvement on contact form

--- src/app/components/contact/service/contact-form.service.ts
+++ src/app/components/contact/service/contact-form.service.ts
@@ -1,25 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Http, Response, ResponseContentType, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ContactFormService {
+
+  private static readonly URL = 'https://uma80bpn0c.execute-api.us-east-1.amazonaws.com/prod/CommunicationGateway';
 
   constructor(private http: Http) {
   }
 
 
-  sendContactForm(data) {
+  sendContactForm(data): Observable<boolean> {
     if (typeof data.name !== 'string' || typeof data.email !== 'string' || typeof data.message !== 'string') {
-      return null;
+      Observable.throw(new Error('error!'));
     }
 
-    this.http.post('https://uma80bpn0c.execute-api.us-east-1.amazonaws.com/prod/CommunicationGateway',{
-        name: data.name,
-        content: data.email,
-        email: data.message
-      }).subscribe((response: Response) => {
-        console.log(response);
-      });
-    return true;
+    return this.http.post(ContactFormService.URL, {
+      name: data.name,
+      content: data.message,
+      email: data.email
+    }).map((value) => {
+      return true;
+    }).catch((err) => {
+      // Maybe we could add error message or something
+      throw new Error;
+    })
+
   }
 }
