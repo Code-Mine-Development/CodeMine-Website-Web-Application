e2c6ad2393aa74bb397fced9fa8ba601c8236a6e
mateuszwit <maateew@gmail.com>
2017-09-20 08:40:14 +0200

    funny images

--- src/app/components/contact/service/contact-form.service.ts
+++ src/app/components/contact/service/contact-form.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Response, ResponseContentType, Headers} from '@angular/http';
+import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class ContactFormService {
@@ -13,13 +13,13 @@ export class ContactFormService {
       return null;
     }
 
-    this.http.post('https://uma80bpn0c.execute-api.us-east-1.amazonaws.com/prod/CommunicationGateway',{
-        name: data.name,
-        content: data.email,
-        email: data.message
-      }).subscribe((response: Response) => {
-        console.log(response);
-      });
+    this.http.post('https://uma80bpn0c.execute-api.us-east-1.amazonaws.com/prod/CommunicationGateway', {
+      name: data.name,
+      content: data.email,
+      email: data.message
+    }).subscribe((response: Response) => {
+      console.log(response);
+    });
     return true;
   }
 }
